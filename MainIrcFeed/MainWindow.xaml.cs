f44b271de94a7fa5b90e7e631080f00740f4a195
Adis <adis.kovacevic@mavs.uta.edu>
2016-05-01 14:57:54 -0500

    removed unsused code

--- MainIrcFeed/MainWindow.xaml.cs
+++ MainIrcFeed/MainWindow.xaml.cs
@@ -61,22 +61,6 @@ namespace MainIrcFeed
             client.LocalUser.LeftChannel += IrcClient_LocalUser_LeftChannel;
         }
 
-        private void IrcClient_LocalUser_LeftChannel(object sender, IrcChannelEventArgs e)
-        {
-            var localUser = (IrcLocalUser)sender;
-
-            e.Channel.UserJoined -= IrcClient_Channel_UserJoined;
-            e.Channel.UserLeft -= IrcClient_Channel_UserLeft;
-            e.Channel.MessageReceived -= IrcClient_Channel_MessageReceived;
-            e.Channel.NoticeReceived -= IrcClient_Channel_NoticeReceived;
-
-            Console.WriteLine("You left the channel {0}.", e.Channel.Name);
-            App.Current.Dispatcher.Invoke((Action)delegate
-            {
-                info.Content = String.Format("You left the channel {0}", e.Channel.Name);
-            });
-        }
-
         private void IrcClient_LocalUser_JoinedChannel(object sender, IrcChannelEventArgs e)
         {
             var localUser = (IrcLocalUser)sender;
@@ -87,7 +71,6 @@ namespace MainIrcFeed
             e.Channel.MessageReceived += IrcClient_Channel_MessageReceived;
             e.Channel.NoticeReceived += IrcClient_Channel_NoticeReceived;
 
-            Console.WriteLine("You joined the channel {0}.", e.Channel.Name);
             App.Current.Dispatcher.Invoke((Action)delegate
             {
                 info.Content = String.Format("You Joined the channel {0}", e.Channel.Name);
@@ -101,7 +84,6 @@ namespace MainIrcFeed
         {
             var channel = (IrcChannel)sender;
 
-            Console.WriteLine("[{0}] Notice: {1}.", channel.Name, e.Text);
 
             App.Current.Dispatcher.Invoke((Action)delegate
             {
@@ -117,9 +99,6 @@ namespace MainIrcFeed
                 if (e.Text.IndexOf("lol", StringComparison.InvariantCultureIgnoreCase) >= 0 || e.Text.Equals("a",StringComparison.InvariantCultureIgnoreCase))
                 {
                     InputReader.SendInputChar('x');
-                    //_botSender.SendMessage(_superlaggkingChannel,"A");
-                    _writeToConsole(e.Source.Name, "A");
-
                     App.Current.Dispatcher.Invoke((Action)delegate
                     {
                         _inputs.Insert(0,new Input() { Username = e.Source.Name, ButtonImage = Images.A});
@@ -129,8 +108,6 @@ namespace MainIrcFeed
                 if (e.Text.IndexOf("yea", StringComparison.InvariantCultureIgnoreCase) >= 0 || e.Text.Equals("b", StringComparison.InvariantCultureIgnoreCase))
                 {
                     InputReader.SendInputChar('c');
-                    //_botSender.SendMessage(_superlaggkingChannel, "B");
-                    _writeToConsole(e.Source.Name, "B");
                     App.Current.Dispatcher.Invoke((Action)delegate
                     {
                         _inputs.Insert(0,new Input() { Username = e.Source.Name, ButtonImage = Images.B });
@@ -140,8 +117,6 @@ namespace MainIrcFeed
                 if (e.Text.IndexOf("ayy", StringComparison.InvariantCultureIgnoreCase) >= 0 || e.Text.Equals("dl", StringComparison.InvariantCultureIgnoreCase))
                 {
                     InputReader.SendInputChar('4');
-                    //_botSender.SendMessage(_superlaggkingChannel, "D-pad Left");
-                    _writeToConsole(e.Source.Name, "D-pad Left");
 
                     App.Current.Dispatcher.Invoke((Action)delegate
                     {
@@ -152,8 +127,6 @@ namespace MainIrcFeed
                 if (e.Text.IndexOf("nice", StringComparison.InvariantCultureIgnoreCase) >= 0 || e.Text.Equals("dr", StringComparison.InvariantCultureIgnoreCase))
                 {
                     InputReader.SendInputChar('6');
-                    //_botSender.SendMessage(_superlaggkingChannel, "D-pad Right" );
-                    _writeToConsole(e.Source.Name, "D-pad Right");
                     App.Current.Dispatcher.Invoke((Action)delegate
                     {
                         _inputs.Insert(0, new Input() { Username = e.Source.Name, ButtonImage = Images.Dr });
@@ -163,8 +136,6 @@ namespace MainIrcFeed
                 if (e.Text.IndexOf("rip", StringComparison.InvariantCultureIgnoreCase) >= 0 || e.Text.Equals("du", StringComparison.InvariantCultureIgnoreCase))
                 {
                     InputReader.SendInputChar('8');
-                    //_botSender.SendMessage(_superlaggkingChannel, "D-pad Up");
-                    _writeToConsole(e.Source.Name, "D-pad Up");
                     App.Current.Dispatcher.Invoke((Action)delegate
                     {
                         _inputs.Insert(0, new Input() { Username = e.Source.Name, ButtonImage = Images.Du });
@@ -174,8 +145,6 @@ namespace MainIrcFeed
                 if (e.Text.IndexOf("ohh", StringComparison.InvariantCultureIgnoreCase) >= 0 || e.Text.Equals("dd", StringComparison.InvariantCultureIgnoreCase))
                 {
                     InputReader.SendInputChar('2');
-                    //_botSender.SendMessage(_superlaggkingChannel, "D-pad Down");
-                    _writeToConsole(e.Source.Name, "D-pad Down");
                     App.Current.Dispatcher.Invoke((Action)delegate
                     {
                         _inputs.Insert(0, new Input() { Username = e.Source.Name, ButtonImage = Images.Dd });
@@ -185,8 +154,6 @@ namespace MainIrcFeed
                 if (e.Text.IndexOf("woo", StringComparison.InvariantCultureIgnoreCase) >= 0 || e.Text.Equals("cl", StringComparison.InvariantCultureIgnoreCase))
                 {
                     InputReader.SendInputChar('j');
-                    //_botSender.SendMessage(_superlaggkingChannel, "D-pad Left");
-                    _writeToConsole(e.Source.Name, "C-Left");
                     App.Current.Dispatcher.Invoke((Action)delegate
                     {
                         _inputs.Insert(0, new Input() { Username = e.Source.Name, ButtonImage = Images.Cl });
@@ -196,8 +163,6 @@ namespace MainIrcFeed
                 if (e.Text.IndexOf("hi", StringComparison.InvariantCultureIgnoreCase) >= 0 || e.Text.Equals("cr", StringComparison.InvariantCultureIgnoreCase))
                 {
                     InputReader.SendInputChar('l');
-                    //_botSender.SendMessage(_superlaggkingChannel, "D-pad Right");
-                    _writeToConsole(e.Source.Name, "C-Right");
                     App.Current.Dispatcher.Invoke((Action)delegate
                     {
                         _inputs.Insert(0, new Input() { Username = e.Source.Name, ButtonImage = Images.Cr });
@@ -207,8 +172,6 @@ namespace MainIrcFeed
                 if (e.Text.IndexOf("gg", StringComparison.InvariantCultureIgnoreCase) >= 0 || e.Text.Equals("cu", StringComparison.InvariantCultureIgnoreCase))
                 {
                     InputReader.SendInputChar('i');
-                    //_botSender.SendMessage(_superlaggkingChannel, "C- Up");
-                    _writeToConsole(e.Source.Name, "C-UP");
                     App.Current.Dispatcher.Invoke((Action)delegate
                     {
                         _inputs.Insert(0, new Input() { Username = e.Source.Name, ButtonImage = Images.Cu });
@@ -218,8 +181,6 @@ namespace MainIrcFeed
                 if (e.Text.IndexOf("wtf", StringComparison.InvariantCultureIgnoreCase) >= 0 || e.Text.Equals("cd", StringComparison.InvariantCultureIgnoreCase))
                 {
                     InputReader.SendInputChar('k');
-                    //_botSender.SendMessage(_superlaggkingChannel, "C Down");
-                    _writeToConsole(e.Source.Name, "C-Down");
                     App.Current.Dispatcher.Invoke((Action)delegate
                     {
                         _inputs.Insert(0, new Input() { Username = e.Source.Name, ButtonImage = Images.Cd });
@@ -229,8 +190,6 @@ namespace MainIrcFeed
                 if (e.Text.IndexOf("ily", StringComparison.InvariantCultureIgnoreCase) >= 0 || e.Text.Equals("z", StringComparison.InvariantCultureIgnoreCase))
                 {
                     InputReader.SendInputChar('z');
-                    //_botSender.SendMessage(_superlaggkingChannel, "Z");
-                    _writeToConsole(e.Source.Name, "Z");
                     App.Current.Dispatcher.Invoke((Action)delegate
                     {
                         _inputs.Insert(0, new Input() { Username = e.Source.Name, ButtonImage = Images.Z });
@@ -240,8 +199,6 @@ namespace MainIrcFeed
                 if (e.Text.IndexOf("omg", StringComparison.InvariantCultureIgnoreCase) >= 0 || e.Text.Equals("al", StringComparison.InvariantCultureIgnoreCase))
                 {
                     InputReader.SendInputChar((char)37);
-                    //_botSender.SendMessage(_superlaggkingChannel, "Analog Left");
-                    _writeToConsole(e.Source.Name, "Analog Left");
                     App.Current.Dispatcher.Invoke((Action)delegate
                     {
                         _inputs.Insert(0, new Input() { Username = e.Source.Name, ButtonImage = Images.Dl });
@@ -251,8 +208,6 @@ namespace MainIrcFeed
                 if (e.Text.IndexOf("savage", StringComparison.InvariantCultureIgnoreCase) >= 0 || e.Text.Equals("ar", StringComparison.InvariantCultureIgnoreCase))
                 {
                     InputReader.SendInputChar((char)39);
-                    //_botSender.SendMessage(_superlaggkingChannel, "Analog Right");
-                    _writeToConsole(e.Source.Name, "Analog Right");
                     App.Current.Dispatcher.Invoke((Action)delegate
                     {
                         _inputs.Insert(0, new Input() { Username = e.Source.Name, ButtonImage = Images.Dr });
@@ -262,8 +217,6 @@ namespace MainIrcFeed
                 if (e.Text.IndexOf("lmao", StringComparison.InvariantCultureIgnoreCase) >= 0 || e.Text.Equals("au", StringComparison.InvariantCultureIgnoreCase))
                 {
                     InputReader.SendInputChar((char)38);
-                    //_botSender.SendMessage(_superlaggkingChannel, "Analog Up");
-                    _writeToConsole(e.Source.Name, "Analog Up");
                     App.Current.Dispatcher.Invoke((Action)delegate
                     {
                         _inputs.Insert(0, new Input() { Username = e.Source.Name, ButtonImage = Images.Du });
@@ -273,8 +226,6 @@ namespace MainIrcFeed
                 if (e.Text.IndexOf("lul", StringComparison.InvariantCultureIgnoreCase) >= 0 || e.Text.Equals("ad", StringComparison.InvariantCultureIgnoreCase))
                 {
                     InputReader.SendInputChar((char)40);
-                    //_botSender.SendMessage(_superlaggkingChannel, "analog Down");
-                    _writeToConsole(e.Source.Name, "Analog Down");
                     App.Current.Dispatcher.Invoke((Action)delegate
                     {
                         _inputs.Insert(0, new Input() { Username = e.Source.Name, ButtonImage = Images.Dd });
@@ -284,8 +235,6 @@ namespace MainIrcFeed
                 if (e.Text.IndexOf("wow", StringComparison.InvariantCultureIgnoreCase) >= 0 || e.Text.Equals("start", StringComparison.InvariantCultureIgnoreCase))
                 {
                     InputReader.SendInputChar((char)13);
-                    //_botSender.SendMessage(_superlaggkingChannel, "Start");
-                    _writeToConsole(e.Source.Name, "Start");
                     App.Current.Dispatcher.Invoke((Action)delegate
                     {
                         _inputs.Insert(0, new Input() { Username = e.Source.Name, ButtonImage = Images.Start });
@@ -295,8 +244,6 @@ namespace MainIrcFeed
                 if (e.Text.IndexOf("omfg", StringComparison.InvariantCultureIgnoreCase) >= 0 || e.Text.Equals("l", StringComparison.InvariantCultureIgnoreCase))
                 {
                     InputReader.SendInputChar('a');
-                    //_botSender.SendMessage(_superlaggkingChannel, "L");
-                    _writeToConsole(e.Source.Name, "L");
                     App.Current.Dispatcher.Invoke((Action)delegate
                     {
                         _inputs.Insert(0, new Input() { Username = e.Source.Name, ButtonImage = Images.L });
@@ -306,8 +253,6 @@ namespace MainIrcFeed
                 if (e.Text.IndexOf("fag", StringComparison.InvariantCultureIgnoreCase) >= 0 || e.Text.Equals("r", StringComparison.InvariantCultureIgnoreCase))
                 {
                     InputReader.SendInputChar('s');
-                    //_botSender.SendMessage(_superlaggkingChannel, "R");
-                    _writeToConsole(e.Source.Name, "R");
                     App.Current.Dispatcher.Invoke((Action)delegate
                     {
                         _inputs.Insert(0, new Input() { Username = e.Source.Name, ButtonImage = Images.R });
@@ -338,17 +283,33 @@ namespace MainIrcFeed
                 //Console.WriteLine("[{0}]({1}) Message: {2}", channel.Name, e.Source.Name, e.Text);
             }
         }
+        #region unused
+
+        private void IrcClient_LocalUser_LeftChannel(object sender, IrcChannelEventArgs e)
+        {
+            var localUser = (IrcLocalUser)sender;
+
+            e.Channel.UserJoined -= IrcClient_Channel_UserJoined;
+            e.Channel.UserLeft -= IrcClient_Channel_UserLeft;
+            e.Channel.MessageReceived -= IrcClient_Channel_MessageReceived;
+            e.Channel.NoticeReceived -= IrcClient_Channel_NoticeReceived;
+
+            App.Current.Dispatcher.Invoke((Action)delegate
+            {
+                info.Content = String.Format("You left the channel {0}", e.Channel.Name);
+            });
+        }
 
         private void IrcClient_Channel_UserLeft(object sender, IrcChannelUserEventArgs e)
         {
             var channel = (IrcChannel)sender;
-            Console.WriteLine("[{0}] User {1} left the channel.", channel.Name, e.ChannelUser.User.NickName);
+            //Console.WriteLine("[{0}] User {1} left the channel.", channel.Name, e.ChannelUser.User.NickName);
         }
 
         private void IrcClient_Channel_UserJoined(object sender, IrcChannelUserEventArgs e)
         {
             var channel = (IrcChannel)sender;
-            Console.WriteLine("[{0}] User {1} joined the channel.", channel.Name, e.ChannelUser.User.NickName);
+            //Console.WriteLine("[{0}] User {1} joined the channel.", channel.Name, e.ChannelUser.User.NickName);
         }
 
         private void IrcClient_LocalUser_MessageReceived(object sender, IrcMessageEventArgs e)
@@ -356,18 +317,18 @@ namespace MainIrcFeed
             if (e.Source is IrcUser)
             {
                 // Read message.
-                Console.WriteLine("({0}): {1}.", e.Source.Name, e.Text);
+                //Console.WriteLine("({0}): {1}.", e.Source.Name, e.Text);
             }
             else
             {
-                Console.WriteLine("({0}) Message: {1}.", e.Source.Name, e.Text);
+                //Console.WriteLine("({0}) Message: {1}.", e.Source.Name, e.Text);
             }
         }
 
         private void IrcClient_LocalUser_NoticeReceived(object sender, IrcMessageEventArgs e)
         {
             var localUser = (IrcLocalUser)sender;
-            Console.WriteLine("Notice: {0}.", e.Text);
+            //Console.WriteLine("Notice: {0}.", e.Text);
         }
 
         private static void IrcClient_Disconnected(object sender, EventArgs e)
@@ -380,18 +341,11 @@ namespace MainIrcFeed
             var client = (IrcClient)sender;
         }
 
-        private static void _writeToConsole(string name, string text)
-        {
-            Console.ForegroundColor = ConsoleColor.DarkGreen;
-            Console.Write("[" + name + "]");
-            Console.ForegroundColor = ConsoleColor.Blue;
-            Console.WriteLine(" : " + text);
-        }
-
         private void textBox_TextChanged(object sender, TextChangedEventArgs e)
         {
 
         }
+#endregion
 
         private void pause_Click(object sender, RoutedEventArgs e)
         {
@@ -436,19 +390,19 @@ namespace MainIrcFeed
                             });
                         if (!connectedEvent.Wait(10000))
                         {
-                            Console.WriteLine("Connection to '{0}' timed out.", server);
+                            info.Content = string.Format("Connection to '{0}' timed out", server);
                             return;
                         }
                     }
-                    Console.Out.WriteLine("Now connected to '{0}'.", server);
+
+                    info.Content = string.Format("Now connected to '{0}'", server);
                     if (!registeredEvent.Wait(10000))
                     {
-                        Console.WriteLine("Could not register to '{0}'.", server);
+                        info.Content = string.Format("Could not register to '{0}'", server);
                         return;
                     }
                 }
 
-                Console.Out.WriteLine("Now registered to '{0}' as '{1}'.", server, username);
                 info.Content = string.Format("Now Registered to {0} as {1}", server, username);
                 disconnect.Content = "Disconnect";
             }
